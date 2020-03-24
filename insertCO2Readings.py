4a3e9743c7f76e246b9b3aaed3de19abd32b98aa
e013988g <62615817+e013988g@users.noreply.github.com>
2020-03-24 18:39:04 +0000

    gh

--- insertCO2Readings.py
+++ insertCO2Readings.py
@@ -4,13 +4,3 @@ database = 'lukefyp'
 username = 'e013988g'
 password = 'lukefyp2020!'
 print(pyodbc.drivers())
-
-drivers = [item for item in pyodbc.drivers()]
-driver = drivers[-1]
-cnxn = pyodbc.connect('DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password)
-cursor = cnxn.cursor()
-cursor.execute("SELECT TOP 20 FROM Device_Details")
-row = cursor.fetchone()
-while row:
-    print (str(row[0]) + " " + str(row[1]))
-    row = cursor.fetchone()
