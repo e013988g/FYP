93e865a50b6acead904ecda3c7893928190dbf38
e013988g <62615817+e013988g@users.noreply.github.com>
2020-03-24 18:53:28 +0000

    d

--- insertCO2Readings.py
+++ insertCO2Readings.py
@@ -3,4 +3,10 @@ server = 'e013988g.database.windows.net'
 database = 'lukefyp'
 username = 'e013988g'
 password = 'lukefyp2020!'
-print(pyodbc.drivers())
+cnxn = pyodbc.connect('SERVER='+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password)
+cursor = cnxn.cursor()
+cursor.execute("SELECT TOP 20 FROM Device_Details")
+row = cursor.fetchone()
+while row:
+    print (str(row[0]) + " " + str(row[1]))
+    row = cursor.fetchone()
