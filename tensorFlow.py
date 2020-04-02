d48c8e60825e97906ac58750c60d4b9401804ad5
e013988g <62615817+e013988g@users.noreply.github.com>
2020-04-02 20:34:54 +0100

    Update tensorFlow.py
    d

--- tensorFlow.py
+++ tensorFlow.py
@@ -1,9 +1,9 @@
-import tensorflow as tf;
-import matplotlib as mpl;
-import matplotlib.pyplot as plt;
-import numpy as np;
-import os;
-import pandas as pd;
+import tensorflow as tf
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import pandas as pd
 mpl.rcParams['figure.figsize'] = (8, 6)
 mpl.rcParams['axes.grid'] = False
 zip_path = tf.keras.utils.get_file(origin = 'https://storage.googleapis.com/tensorflow/tf-keras-datasets/jena_climate_2009_2016.csv.zip', fname = 'jena_climate_2009_2016.csv.zip', extract = True)
