fae3f71f9df6c149c0e1f2249fa6b35609fad2dd
e013988g <62615817+e013988g@users.noreply.github.com>
2020-04-02 21:05:30 +0100

    Update tensorFlow.py
    d

--- tensorFlow.py
+++ tensorFlow.py
@@ -46,4 +46,6 @@ def univariate_data(dataset, start_index, end_index, history_size, target_size):
         indices = range(i - history_size, i)# Reshape data from(history_size, ) to(history_size, 1)
         data.append(np.reshape(dataset[indices], (history_size, 1)))
         labels.append(dataset[i + target_size])
+        
+        
     return np.array(data), np.array(labels)
