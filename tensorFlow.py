1008d7bcec9518caddd056fc099f88f08e5ccd81
e013988g <62615817+e013988g@users.noreply.github.com>
2020-04-02 21:01:22 +0100

    Update tensorFlow.py
    d

--- tensorFlow.py
+++ tensorFlow.py
@@ -35,15 +35,15 @@ print ('\n Target temperature to predict')
 print (y_train_uni[0])
 
 def univariate_data(dataset, start_index, end_index, history_size, target_size):
-  data = []
-labels = []
+    data = []
+    labels = []
 
-start_index = start_index + history_size
-if end_index is None:
-  end_index = len(dataset) - target_size
+    start_index = start_index + history_size
+    if end_index is None:
+        end_index = len(dataset) - target_size
 
-for i in range(start_index, end_index):
-  indices = range(i - history_size, i)# Reshape data from(history_size, ) to(history_size, 1)
-data.append(np.reshape(dataset[indices], (history_size, 1)))
-labels.append(dataset[i + target_size])
-return np.array(data), np.array(labels)
+    for i in range(start_index, end_index):
+        indices = range(i - history_size, i)# Reshape data from(history_size, ) to(history_size, 1)
+        data.append(np.reshape(dataset[indices], (history_size, 1)))
+        labels.append(dataset[i + target_size])
+    return np.array(data), np.array(labels)
