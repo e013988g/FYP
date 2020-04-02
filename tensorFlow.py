fd595c18d3ff473ab0ec7990ff9b9b7d9d063411
e013988g <62615817+e013988g@users.noreply.github.com>
2020-04-02 21:12:47 +0100

    Update tensorFlow.py
    f

--- tensorFlow.py
+++ tensorFlow.py
@@ -4,6 +4,23 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 import pandas as pd
+
+def univariate_data(dataset, start_index, end_index, history_size, target_size):
+    data = []
+    labels = []
+
+    start_index = start_index + history_size
+    if end_index is None:
+        end_index = len(dataset) - target_size
+
+    for i in range(start_index, end_index):
+        indices = range(i - history_size, i)# Reshape data from(history_size, ) to(history_size, 1)
+        data.append(np.reshape(dataset[indices], (history_size, 1)))
+        labels.append(dataset[i + target_size])
+        
+        
+    return np.array(data), np.array(labels)
+
 mpl.rcParams['figure.figsize'] = (8, 6)
 mpl.rcParams['axes.grid'] = False
 zip_path = tf.keras.utils.get_file(origin = 'https://storage.googleapis.com/tensorflow/tf-keras-datasets/jena_climate_2009_2016.csv.zip', fname = 'jena_climate_2009_2016.csv.zip', extract = True)
@@ -23,29 +40,13 @@ uni_data = (uni_data-uni_train_mean)/uni_train_std
 univariate_past_history = 20
 univariate_future_target = 0
 
-x_train_uni, y_train_uni = self.univariate_data(uni_data, 0, TRAIN_SPLIT,
+x_train_uni, y_train_uni = univariate_data(uni_data, 0, TRAIN_SPLIT,
                                            univariate_past_history,
                                            univariate_future_target)
-x_val_uni, y_val_uni = self.univariate_data(uni_data, TRAIN_SPLIT, None,
+x_val_uni, y_val_uni = univariate_data(uni_data, TRAIN_SPLIT, None,
                                        univariate_past_history,
                                        univariate_future_target)
 print ('Single window of past history')
 print (x_train_uni[0])
 print ('\n Target temperature to predict')
 print (y_train_uni[0])
-
-def univariate_data(dataset, start_index, end_index, history_size, target_size):
-    data = []
-    labels = []
-
-    start_index = start_index + history_size
-    if end_index is None:
-        end_index = len(dataset) - target_size
-
-    for i in range(start_index, end_index):
-        indices = range(i - history_size, i)# Reshape data from(history_size, ) to(history_size, 1)
-        data.append(np.reshape(dataset[indices], (history_size, 1)))
-        labels.append(dataset[i + target_size])
-        
-        
-    return np.array(data), np.array(labels)
