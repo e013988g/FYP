f384f457bf8f34e2c1167d86206306349cee5534
e013988g <62615817+e013988g@users.noreply.github.com>
2020-04-02 21:17:54 +0100

    Update tensorFlow.py
    dd

--- tensorFlow.py
+++ tensorFlow.py
@@ -21,6 +21,30 @@ def univariate_data(dataset, start_index, end_index, history_size, target_size):
         
     return np.array(data), np.array(labels)
 
+def create_time_steps(length):
+  return list(range(-length, 0))
+
+def show_plot(plot_data, delta, title):
+  labels = ['History', 'True Future', 'Model Prediction']
+  marker = ['.-', 'rx', 'go']
+  time_steps = create_time_steps(plot_data[0].shape[0])
+  if delta:
+    future = delta
+  else:
+    future = 0
+
+  plt.title(title)
+  for i, x in enumerate(plot_data):
+    if i:
+      plt.plot(future, plot_data[i], marker[i], markersize=10,
+               label=labels[i])
+    else:
+      plt.plot(time_steps, plot_data[i].flatten(), marker[i], label=labels[i])
+  plt.legend()
+  plt.xlim([time_steps[0], (future+5)*2])
+  plt.xlabel('Time-Step')
+  return plt
+
 mpl.rcParams['figure.figsize'] = (8, 6)
 mpl.rcParams['axes.grid'] = False
 zip_path = tf.keras.utils.get_file(origin = 'https://storage.googleapis.com/tensorflow/tf-keras-datasets/jena_climate_2009_2016.csv.zip', fname = 'jena_climate_2009_2016.csv.zip', extract = True)
@@ -50,3 +74,4 @@ print ('Single window of past history')
 print (x_train_uni[0])
 print ('\n Target temperature to predict')
 print (y_train_uni[0])
+show_plot([x_train_uni[0], y_train_uni[0]], 0, 'Sample Example')
