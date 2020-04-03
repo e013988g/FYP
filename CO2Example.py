8aeb73bfa0518a0497075f74716efa03906d751c
e013988g <62615817+e013988g@users.noreply.github.com>
2020-04-03 08:27:20 +0100

    Update CO2Example.py
    d

--- CO2Example.py
+++ CO2Example.py
@@ -2,18 +2,21 @@ from mqCO2 import *
 from mqCO import *
 from sendCO2Text import *
 from sendCO2Email import *
+from sendCOText import *
 import sys, time
 
 try:
     CO2Trigerred = False
     PreviousTriggeredState = False
+    COPreviousTriggeredState = False
     mqCO2 = MQCO2();
-	mqCO = MQCO();
+    mqCO = MQCO();
     CO2Text = sendCO2Text();
+    COText = sendCOText();
     sendCarbonDioxideEmail = sendCO2Email();
     while True:
             CO2Perc = mqCO2.MQPercentage()
-            COPerc = mqCO.MQPercentage()        			
+            COPerc = mqCO.MQPercentage()                    
             sys.stdout.write("\r")
             sys.stdout.write("\033[K")
             sys.stdout.write("CO2: %g ppm, CO: %g ppm" % ((CO2Perc["SMOKE"] * 20000), (COPerc["CO"])))
@@ -27,7 +30,14 @@ try:
             
             if ((CO2Perc["SMOKE"]) * 20000) <= 1000:
                 previousTriggeredState = False
-                
+            
+            if COPerc > 70:
+                if COPreviousTriggeredState == False:
+                    message = COText.createClientMessage()
+                    previousTriggeredState = True
+            
+            if COPerc <= 70:
+                previousTriggeredState = False    
 except:
     e = sys.exc_info()[0]
     print("error: %s" % e)
