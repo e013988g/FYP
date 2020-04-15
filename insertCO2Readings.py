c7c0ff20f5d53f3d05d743461f6889696c065f61
e013988g <62615817+e013988g@users.noreply.github.com>
2020-04-15 19:21:03 +0100

    Update insertCO2Readings.py
    d

--- insertCO2Readings.py
+++ insertCO2Readings.py
@@ -1,12 +1,11 @@
-import pyodbc
-print(pyodbc)
-server = 'e013988g.database.windows.net'
-database = 'lukefyp'
+import jaydebeapi
+
 username = 'e013988g'
 password = 'lukefyp2020!'
 driver= '/usr/local/lib/python2.7/dist-packages/pyodbc.so'
+conn = jaydebeapi.connect("org.hsqldb.jdbcDriver", "jdbc:hsqldb:mem:e013988g.database.windows.net/lukefyp", [username, password], "/path/to/hsqldb.jar")
 cnxn = pyodbc.connect('DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password)
-cursor = cnxn.cursor()
+curs = conn.cursor()
 cursor.execute("SELECT TOP 20 FROM Device_Details")
 row = cursor.fetchone()
 while row:
