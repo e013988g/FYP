18ea6fd9c308e1f1825f2d434991e3e32ba57710
e013988g <62615817+e013988g@users.noreply.github.com>
2020-04-15 19:28:07 +0100

    Update insertCO2Readings.py
    d

--- insertCO2Readings.py
+++ insertCO2Readings.py
@@ -1,10 +1,9 @@
 import pyodbc
-print(pyodbc)
 server = 'e013988g.database.windows.net'
 database = 'lukefyp'
 username = 'e013988g'
 password = 'lukefyp2020!'
-driver= '{ODBC Driver 17 for SQL Server}'
+driver= 'FreeTDS'
 cnxn = pyodbc.connect('DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password)
 cursor = cnxn.cursor()
 cursor.execute("SELECT TOP 20 FROM Device_Details")
