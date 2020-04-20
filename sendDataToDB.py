c36ee7a4a91022e47a7b069849b786583f1872c7
e013988g <62615817+e013988g@users.noreply.github.com>
2020-04-20 01:41:40 +0100

    Update sendDataToDB.py
    d

--- sendDataToDB.py
+++ sendDataToDB.py
@@ -2,19 +2,30 @@ import pyodbc
 import datetime
 class insertReading():
     def insertCO2Reading(ReadingPPM):
-        conn = pyodbc.connect('DRIVER=FreeTDS;SERVER=e013988g.database.windows.net;PORT=1433;DATABASE=learpfyp;UID=e013988g;PWD=lukefyp2020!;TDS_Version=8.0;')
-        cursor = conn.cursor()
-        now = datetime.datetime.now()
-        now = now.strftime("%Y-%m-%d %H:%M:%S.%s.%f")[:-3]
-        sql_text= "INSERT INTO CO2_Readings (DeviceID, ReadingPPM, DateRegistered) VALUES (1," + ReadingPPM + "," + now  + ")"
-        cursor.execute(sql_text)
+        try:
+            conn = pyodbc.connect('DRIVER=FreeTDS;SERVER=e013988g.database.windows.net;PORT=1433;DATABASE=learpfyp;UID=e013988g;PWD=lukefyp2020!;TDS_Version=8.0;')
+            cursor = conn.cursor()
+            now = datetime.datetime.now()
+            now = now.strftime("%Y-%m-%d %H:%M:%S.%s.%f")[:-3]
+            sql_text= "INSERT INTO CO2_Readings (DeviceID, ReadingPPM, DateRegistered) VALUES (1," + ReadingPPM + "," + now  + ")"
+            cursor.execute(sql_text)
+            print("affected rows = {}".format(cursor.rowcount))
+        except:
+            e = sys.exc_info()[0]
+            print("error: %s" % e)
+            
         
     def insertCOReading(ReadingPPM):
-        conn = pyodbc.connect('DRIVER=FreeTDS;SERVER=e013988g.database.windows.net;PORT=1433;DATABASE=learpfyp;UID=e013988g;PWD=lukefyp2020!;TDS_Version=8.0;')
-        cursor = conn.cursor()
-        now = datetime.datetime.now()
-        now = now.strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
-        sql_text= "INSERT INTO CO_Readings (DeviceID, ReadingPPM, DateRegistered) VALUES (1," + ReadingPPM + "," + now + ")"
-        cursor.execute(sql_text)
+        try:
+            conn = pyodbc.connect('DRIVER=FreeTDS;SERVER=e013988g.database.windows.net;PORT=1433;DATABASE=learpfyp;UID=e013988g;PWD=lukefyp2020!;TDS_Version=8.0;')
+            cursor = conn.cursor()
+            now = datetime.datetime.now()
+            now = now.strftime("%Y-%m-%d %H:%M:%S.%s.%f")[:-3]
+            sql_text= "INSERT INTO CO2_Readings (DeviceID, ReadingPPM, DateRegistered) VALUES (1," + ReadingPPM + "," + now  + ")"
+            cursor.execute(sql_text)
+            print("affected rows = {}".format(cursor.rowcount))
+        except:
+            e = sys.exc_info()[0]
+            print("error: %s" % e)
 
     
