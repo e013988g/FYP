fb1e9e0fa5e4cbbc7aad5abb03bd8269169e3d1b
e013988g <62615817+e013988g@users.noreply.github.com>
2020-04-20 02:16:47 +0100

    Update sendDataToDB.py
    dd

--- sendDataToDB.py
+++ sendDataToDB.py
@@ -2,7 +2,7 @@ import pyodbc
 import datetime
 
 class insertReading():
-    def insertCO2Reading(self, ReadingPPM):
+    def insertCO2Reading(self):
         try:
             conn = pyodbc.connect('DRIVER=FreeTDS;SERVER=e013988g.database.windows.net;PORT=1433;DATABASE=learpfyp;UID=e013988g;PWD=lukefyp2020!;TDS_Version=8.0;')
             cursor = conn.cursor()
