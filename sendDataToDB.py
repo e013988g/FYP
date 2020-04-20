1b0f07ae3b186f4815eddc281329d6311dd860c2
e013988g <62615817+e013988g@users.noreply.github.com>
2020-04-20 02:58:38 +0100

    Update sendDataToDB.py
    dd

--- sendDataToDB.py
+++ sendDataToDB.py
@@ -8,7 +8,7 @@ class insertReading():
             conn = pyodbc.connect('DRIVER=FreeTDS;SERVER=e013988g.database.windows.net;PORT=1433;DATABASE=learpfyp;UID=e013988g;PWD=lukefyp2020!;TDS_Version=8.0;')
             cursor = conn.cursor()
             now = datetime.datetime.now()
-            #now = now.strftime("%Y-%m-%d %H:%M:%S.%s")
+            now = now.strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
             sql_text = "INSERT INTO CO2_Readings (DeviceID, ReadingPPM, DateRegistered) VALUES (1," + str(ReadingPPM * 20000) + "," + now + ")"
             print(sql_text)
             cursor.execute(sql_text)
