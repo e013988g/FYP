761296876a6d98a0a9b13f2a1dcc0afa6ea4ebf6
e013988g <62615817+e013988g@users.noreply.github.com>
2020-04-20 03:09:08 +0100

    Update sendDataToDB.py
    dd

--- sendDataToDB.py
+++ sendDataToDB.py
@@ -18,3 +18,17 @@ class insertReading():
             e = sys.exc_info()[1]
             print("error: %s" % e)
             
+    def insertCOReading(self, ReadingPPM):
+        try:
+            conn = pyodbc.connect('DRIVER=FreeTDS;SERVER=e013988g.database.windows.net;PORT=1433;DATABASE=learpfyp;UID=e013988g;PWD=lukefyp2020!;TDS_Version=8.0;')
+            cursor = conn.cursor()
+            now = datetime.datetime.now()
+            now = now.strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
+            sql_text = "INSERT INTO CO_Readings (DeviceID, ReadingPPM, DateRegistered) VALUES (1," + str(ReadingPPM) + "," + "'" + now + "'" + ")"
+            print(sql_text)
+            cursor.execute(sql_text)
+            conn.commit()
+            conn.close()
+        except:
+            e = sys.exc_info()[1]
+            print("error: %s" % e)
