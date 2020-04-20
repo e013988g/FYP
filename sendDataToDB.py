39ee05d17214182a9feb9cf8c35ac016c43ea9b0
e013988g <62615817+e013988g@users.noreply.github.com>
2020-04-20 01:51:13 +0100

    Update sendDataToDB.py
    d

--- sendDataToDB.py
+++ sendDataToDB.py
@@ -4,7 +4,7 @@ import datetime
 class insertReading():
     def insertCO2Reading(ReadingPPM):
         try:
-            conn = pyodbc.connect('DRIVER=FreeTDS;SERVER=e013988g.database.windows.net;PORT=1433;DATABASE=learpfyp;UID=e013988g;PWD=lukefyp2020!;TDS_Version=8.0;')
+            #conn = pyodbc.connect('DRIVER=FreeTDS;SERVER=e013988g.database.windows.net;PORT=1433;DATABASE=learpfyp;UID=e013988g;PWD=lukefyp2020!;TDS_Version=8.0;')
             #cursor = conn.cursor()
             #now = datetime.datetime.now()
             #now = now.strftime("%Y-%m-%d %H:%M:%S.%s.%f")[:-3]
@@ -12,7 +12,7 @@ class insertReading():
             #cursor.execute(sql_text)
             #conn.commit()
             #cursor.close()
-            conn.close()
+            #conn.close()
         except:
             e = sys.exc_info()[0]
             print("error: %s" % e)
