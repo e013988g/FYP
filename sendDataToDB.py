fb35944ca630e017bd3be7bec51d9cd260e031be
e013988g <62615817+e013988g@users.noreply.github.com>
2020-04-20 02:33:58 +0100

    Update sendDataToDB.py
    d

--- sendDataToDB.py
+++ sendDataToDB.py
@@ -8,7 +8,7 @@ class insertReading():
             cursor = conn.cursor()
             now = datetime.datetime.now()
             now = now.strftime("%Y-%m-%d %H:%M:%S.%s.%f")[:-3]
-            sql_text= "INSERT INTO CO2_Readings (DeviceID, ReadingPPM, DateRegistered) VALUES (1," + ReadingPPM + ","  + ")"
+            sql_text= "INSERT INTO CO2_Readings (DeviceID, ReadingPPM, DateRegistered) VALUES (1," + "400.55" + "," + now  + ")"
             #cursor.execute(sql_text)
             conn.close()
         except:
