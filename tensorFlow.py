008b775357f4d84ea687f337b486f4fcaaff6420
e013988g <62615817+e013988g@users.noreply.github.com>
2020-04-21 23:20:29 +0100

    Update tensorFlow.py
    dd

--- tensorFlow.py
+++ tensorFlow.py
@@ -70,3 +70,28 @@ mpl.rcParams['figure.figsize'] = (8, 6)
 mpl.rcParams['axes.grid'] = False
 df = pd.read_json(getRecentDatabaseData())
 print(df)
+TRAIN_SPLIT = 300000
+tf.compat.v1.random.set_random_seed(13)
+uni_data = df['reading']
+uni_data.index = df['dateReg']
+uni_data.head()
+uni_data.plot(subplots=True)
+uni_data = uni_data.values
+uni_train_mean = uni_data[:TRAIN_SPLIT].mean()
+uni_train_std = uni_data[:TRAIN_SPLIT].std()
+uni_data = (uni_data-uni_train_mean)/uni_train_std
+univariate_past_history = 6
+univariate_future_target = 0
+
+x_train_uni, y_train_uni = univariate_data(uni_data, 0, TRAIN_SPLIT,
+                                           univariate_past_history,
+                                           univariate_future_target)
+x_val_uni, y_val_uni = univariate_data(uni_data, TRAIN_SPLIT, None,
+                                       univariate_past_history,
+                                       univariate_future_target)
+print ('Single window of past history')
+print (x_train_uni[0])
+print ('\n Target temperature to predict')
+print (y_train_uni[0])
+show_plot([x_train_uni[0], y_train_uni[0]], 0, 'Sample Example')
+plt.show()
