358f9205d0d3e51e2a3da1e0300182c83ddcd5c9
e013988g <62615817+e013988g@users.noreply.github.com>
2020-04-24 16:11:00 +0100

    Update tensor.py
    dd

--- tensor.py
+++ tensor.py
@@ -113,7 +113,7 @@ simple_lstm_model = tf.keras.models.Sequential([
 ])
 
 simple_lstm_model.compile(optimizer='adam', loss='mae')
-iterator = val_univariate.make_one_shot_iterator()
-next_element = iterator.get_next()
-for x, y in next_element:
+tf.executing_eagerly()
+
+for x, y in val_univariate.take(1):
     print(simple_lstm_model.predict(x).shape)
