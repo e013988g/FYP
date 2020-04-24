e7ea51f6a18c0afb0daec0291fe7ad6d03529945
e013988g <62615817+e013988g@users.noreply.github.com>
2020-04-24 16:55:48 +0100

    Update tensor.py
    d

--- tensor.py
+++ tensor.py
@@ -58,7 +58,7 @@ zip_path = tf.keras.utils.get_file(
 csv_path, _ = os.path.splitext(zip_path)
 df = pd.read_csv(csv_path)
 df.head()
-TRAIN_SPLIT = 3000
+TRAIN_SPLIT = 300
 tf.compat.v1.random.set_random_seed(13)
 uni_data = df['T (degC)']
 uni_data.index = df['Date Time']
@@ -68,7 +68,7 @@ uni_data = uni_data.values
 uni_train_mean = uni_data[:TRAIN_SPLIT].mean()
 uni_train_std = uni_data[:TRAIN_SPLIT].std()
 uni_data = (uni_data-uni_train_mean)/uni_train_std
-univariate_past_history = 20
+univariate_past_history = 2
 univariate_future_target = 0
 
 x_train_uni, y_train_uni = univariate_data(uni_data, 0, TRAIN_SPLIT,
@@ -82,8 +82,8 @@ print (x_train_uni[0])
 print ('\n Target temperature to predict')
 print (y_train_uni[0])
 
-BATCH_SIZE = 25
-BUFFER_SIZE = 100
+BATCH_SIZE = 2
+BUFFER_SIZE = 10
 
 train_univariate = tf.data.Dataset.from_tensor_slices((x_train_uni, y_train_uni))
 train_univariate = train_univariate.cache().shuffle(BUFFER_SIZE).batch(BATCH_SIZE).repeat()
