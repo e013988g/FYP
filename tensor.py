1cef26a09acafab32d7bc0949d27a0c6bee4a574
e013988g <62615817+e013988g@users.noreply.github.com>
2020-04-24 17:06:50 +0100

    Update tensor.py
    dd

--- tensor.py
+++ tensor.py
@@ -104,3 +104,8 @@ EPOCHS = 10
 simple_lstm_model.fit(train_univariate, epochs=EPOCHS,
                       steps_per_epoch=EVALUATION_INTERVAL,
                       validation_data=val_univariate, validation_steps=1)
+
+for x, y in val_univariate.take(3):
+  plot = show_plot([x[0].numpy(), y[0].numpy(),
+                    simple_lstm_model.predict(x)[0]], 0, 'Simple LSTM model')
+  plot.show()
