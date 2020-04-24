bf88444b40d4f4cbc78f9fd6ff1b7b9ddae4f85d
e013988g <62615817+e013988g@users.noreply.github.com>
2020-04-24 16:12:27 +0100

    Update tensor.py
    dd

--- tensor.py
+++ tensor.py
@@ -113,7 +113,10 @@ simple_lstm_model = tf.keras.models.Sequential([
 ])
 
 simple_lstm_model.compile(optimizer='adam', loss='mae')
-tf.executing_eagerly()
 
-for x, y in val_univariate.take(1):
-    print(simple_lstm_model.predict(x).shape)
+EVALUATION_INTERVAL = 200
+EPOCHS = 10
+
+simple_lstm_model.fit(train_univariate, epochs=EPOCHS,
+                      steps_per_epoch=EVALUATION_INTERVAL,
+                      validation_data=val_univariate, validation_steps=50)
