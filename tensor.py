6c46cc17b779f29709c2bbc9b32901ccce6caebe
e013988g <62615817+e013988g@users.noreply.github.com>
2020-04-24 16:46:39 +0100

    Update tensor.py
    dd

--- tensor.py
+++ tensor.py
@@ -1,45 +1,25 @@
 import tensorflow as tf
+
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
 import os
 import pandas as pd
-import json
-import pyodbc
-
-def getRecentDatabaseData():
-    line_items = []
-    conn = pyodbc.connect('DRIVER=FreeTDS;SERVER=e013988g.database.windows.net;PORT=1433;DATABASE=learpfyp;UID=e013988g;PWD=lukefyp2020!;TDS_Version=8.0;')
-    cursor = conn.cursor()
-    sql_text = "SELECT TOP 1500 ReadingPPM, DateRegistered FROM CO2_Readings ORDER BY DateRegistered DESC"
-    cursor.execute(sql_text)
-    row = cursor.fetchone()
-    while row:
-        jsonObject = {
-                'reading': str(row[0]),
-                'dateReg': str(row[1])
-            }
-        line_items.append(jsonObject)
-        row = cursor.fetchone()
-    
-    conn.close()
-    
-    return json.dumps(line_items)
 
 def univariate_data(dataset, start_index, end_index, history_size, target_size):
-    data = []
-    labels = []
+  data = []
+  labels = []
 
-    start_index = start_index + history_size
-    if end_index is None:
-        end_index = len(dataset) - target_size
+  start_index = start_index + history_size
+  if end_index is None:
+    end_index = len(dataset) - target_size
 
-    for i in range(start_index, end_index):
-        indices = range(i - history_size, i)# Reshape data from(history_size, ) to(history_size, 1)
-        data.append(np.reshape(dataset[indices], (history_size, 1)))
-        labels.append(dataset[i + target_size])
-        
-    return np.array(data), np.array(labels)
+  for i in range(start_index, end_index):
+    indices = range(i-history_size, i)
+    # Reshape data from (history_size,) to (history_size, 1)
+    data.append(np.reshape(dataset[indices], (history_size, 1)))
+    labels.append(dataset[i+target_size])
+  return np.array(data), np.array(labels)
 
 def create_time_steps(length):
   return list(range(-length, 0))
@@ -70,12 +50,18 @@ def baseline(history):
 
 mpl.rcParams['figure.figsize'] = (8, 6)
 mpl.rcParams['axes.grid'] = False
-df = pd.read_json(getRecentDatabaseData())
-print(df)
-TRAIN_SPLIT = 1499
-tf.compat.v1.random.set_random_seed(13)
-uni_data = df['reading']
-uni_data.index = df['dateReg']
+
+zip_path = tf.keras.utils.get_file(
+    origin='https://storage.googleapis.com/tensorflow/tf-keras-datasets/jena_climate_2009_2016.csv.zip',
+    fname='jena_climate_2009_2016.csv.zip',
+    extract=True)
+csv_path, _ = os.path.splitext(zip_path)
+df = pd.read_csv(csv_path)
+df.head()
+TRAIN_SPLIT = 300000
+tf.random.set_seed(13)
+uni_data = df['T (degC)']
+uni_data.index = df['Date Time']
 uni_data.head()
 uni_data.plot(subplots=True)
 uni_data = uni_data.values
@@ -95,30 +81,12 @@ print ('Single window of past history')
 print (x_train_uni[0])
 print ('\n Target temperature to predict')
 print (y_train_uni[0])
-show_plot([x_train_uni[0], y_train_uni[0], baseline(x_train_uni[0])], 0,
-           'Baseline Prediction Example')
 
 BATCH_SIZE = 256
-BUFFER_SIZE = 1000
+BUFFER_SIZE = 10000
 
 train_univariate = tf.data.Dataset.from_tensor_slices((x_train_uni, y_train_uni))
 train_univariate = train_univariate.cache().shuffle(BUFFER_SIZE).batch(BATCH_SIZE).repeat()
-print(train_univariate)
+
 val_univariate = tf.data.Dataset.from_tensor_slices((x_val_uni, y_val_uni))
 val_univariate = val_univariate.batch(BATCH_SIZE).repeat()
-print(val_univariate)
-
-simple_lstm_model = tf.keras.models.Sequential([
-    tf.keras.layers.LSTM(8, input_shape=x_train_uni.shape[-2:]),
-    tf.keras.layers.Flatten(),
-    tf.keras.layers.Dense(1)
-])
-
-simple_lstm_model.compile(optimizer='adam', loss='mae')
-
-EVALUATION_INTERVAL = 200
-EPOCHS = 10
-
-simple_lstm_model.fit(train_univariate, epochs=EPOCHS,
-                      steps_per_epoch=EVALUATION_INTERVAL,
-                      validation_data=val_univariate, validation_steps=50)
