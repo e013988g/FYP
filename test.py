29b27c3b0420fdd53e7ea9e0b3bb1d8fdf97e1b5
e013988g <62615817+e013988g@users.noreply.github.com>
2020-05-02 02:34:43 +0100

    Create test.py
    dd

---
+++ test.py
@@ -0,0 +1,31 @@
+from mailjet_rest import Client
+import os
+import sys, time
+from datetime import datetime
+
+now = datetime.now()
+date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
+api_key = '01de10d12429fa1f80909ceb234c3da3'
+api_secret = '20fcff66d6358ce7518e1307810a2167'
+mailjet = Client(auth=(api_key, api_secret), version='v3.1')
+data = {
+  'Messages': [
+    {
+      "From": {
+        "Email": "e013988g@student.staffs.ac.uk",
+        "Name": "Luke"
+      },
+      "To": [
+        {
+          "Email": "lukeearp98@outlook.com",
+          "Name": "Luke"
+        }
+      ],
+      "Subject": "CO Warning! " + date_time,
+      "TextPart": "CO levels have breached a safe level. " + date_time,
+      "HTMLPart": "",
+      "CustomID": "sendnow"
+    }
+  ]
+}
+result = mailjet.send.create(data=data)
