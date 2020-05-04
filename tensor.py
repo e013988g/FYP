d3b438447f4e960dc369cb070c3e03954028a858
e013988g <62615817+e013988g@users.noreply.github.com>
2020-05-04 19:53:52 +0100

    Update tensor.py
    dd

--- tensor.py
+++ tensor.py
@@ -35,7 +35,7 @@ train, test = X[0:size], X[size:len(X)]
 history = [x for x in train]
 predictions = list()
 for t in range(len(test)):
-    model = ARIMA(history, order=(5,1,0))
+    model = ARIMA(history['reading'], order=(5,1,0))
     model_fit = model.fit(disp=0)
     output = model_fit.forecast()
     yhat = output[0]
