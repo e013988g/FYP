e6500857a2242e474f867db57e649184a2bcb8e0
e013988g <62615817+e013988g@users.noreply.github.com>
2020-05-08 10:56:20 +0100

    new scripts
    new scripts

---
+++ sendCOAnomalyEmail.py
@@ -0,0 +1,38 @@
+from mailjet_rest import Client
+import os
+import sys, time
+from datetime import datetime
+
+class sendCOAnomalyEmail():
+    def createEmail(self, sendEmail):
+        if sendEmail > 0:
+            now = datetime.now()
+            date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
+            api_key = 'ce9426e7bb33fe2fc0ebf344f67d7a38'
+            api_secret = 'e860031699212a48d2b2d2972b3a56b8'
+            mailjet = Client(auth=(api_key, api_secret), version='v3.1')
+            data = {
+              'Messages': [
+                {
+                  "From": {
+                    "Email": "lukeearpp98@outlook.com",
+                    "Name": "Luke"
+                  },
+                  "To": [
+                    {
+                      "Email": "lukeearp98@outlook.com",
+                      "Name": "Luke"
+                    }
+                  ],
+                  "Subject": "CO level higher than usual!",
+                  "TextPart": "Your carbon monoxide readings are higher than usual, but still within a safe level." + date_time,
+                  "HTMLPart": ""
+                }
+              ]
+            }
+            result = mailjet.send.create(data=data)
+        else:
+            result = []
+        return result
+
+
---
+++ sendCOAnomalyText.py
@@ -0,0 +1,17 @@
+from twilio.rest import Client
+import sys, time
+
+class sendCOAnomalyText():
+    
+    def createClientMessage(self, sendText):
+        if sendText > 0:
+            ACCOUNT_SID = 'ACd1cd65da15d96c2208c441a1f4dd7fa0'
+            AUTH_TOKEN = '35c51c29ccd1475af7bc6a4abaacf127'   
+            client = Client(ACCOUNT_SID, AUTH_TOKEN)
+            message = client.messages.create(body='Your carbon monoxide readings are higher than usual, but still within a safe level.', from_='+441873740096', to='+447746103651')
+        else:
+            message = ""
+            
+        return message
+
+
