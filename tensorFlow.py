f87aca728ad47866c2f6dc33738e92b7b69faece
e013988g <62615817+e013988g@users.noreply.github.com>
2020-05-09 22:53:01 +0100

    Update tensorFlow.py
    dd

--- tensorFlow.py
+++ tensorFlow.py
@@ -5,5 +5,5 @@ def runThread():
     linearRegression = CO2LinearRegression();
     print(linearRegression.checkForAnomaly(3500.45))
     
-x = threading.Thread(target=runThread, args=(1,))
+x = threading.Thread(target=runThread)
 x.start()
