e3f144f02521c535c018e1bbd25977debb1bb944
e013988g <62615817+e013988g@users.noreply.github.com>
2020-05-10 17:27:24 +0100

    Update tensorFlow.py
    dd

--- tensorFlow.py
+++ tensorFlow.py
@@ -15,7 +15,7 @@ def getRecentDatabaseData():
         line_items = []
         conn = pyodbc.connect('DRIVER=FreeTDS;SERVER=e013988g.database.windows.net;PORT=1433;DATABASE=learpfyp;UID=e013988g;PWD=lukefyp2020!;TDS_Version=8.0;')
         cursor = conn.cursor()
-        sql_text = "SELECT TOP 200 ReadingPPM, DateRegistered FROM CO2_Readings WHERE DATEPART(dd,DateRegistered) = DATEPART(dd, GETDATE() - 7) AND DATEPART(hh, DateRegistered) = (DATEPART(hh, GETDATE()) + 1) ORDER BY DateRegistered ASC"
+        sql_text = "SELECT TOP 200 ReadingPPM, DateRegistered FROM CO2_Readings WHERE DATEPART(dd,DateRegistered) = DATEPART(dd, GETDATE() - 7) AND DATEPART(hh, DateRegistered) = (DATEPART(hh, GETDATE()) + 1) ORDER BY DateRegistered DESC"
         cursor.execute(sql_text)
         row = cursor.fetchone()
         while row:
